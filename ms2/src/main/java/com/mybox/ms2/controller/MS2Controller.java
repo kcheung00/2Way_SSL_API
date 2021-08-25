2e13ed4699deb3b42029608ac4d2f782fc448471
kcheung00 <83565694+kcheung00@users.noreply.github.com>
2021-08-24 18:44:38 -0700

    add post sample code

--- ms2/src/main/java/com/mybox/ms2/controller/MS2Controller.java
+++ ms2/src/main/java/com/mybox/ms2/controller/MS2Controller.java
@@ -1,15 +1,38 @@
 package com.mybox.ms2.controller;
 
+import com.mybox.ms2.services.Greeting;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/ms2")
 public class MS2Controller {
-	@RequestMapping(value = "/data", method = RequestMethod.GET)
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		System.out.println("Greeting name - " + name);
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	@RequestMapping(method = RequestMethod.GET, value = "/data")
 	public String getData() {
 		System.out.println("Returning data from MS 2 data method");
 		return "Hello from MS 2 TWO -  data method";
 	}
-}//end of class MS1Controller
+
+	@RequestMapping(method = RequestMethod.POST, value="/sendmsg")
+	@ResponseBody
+	public String sendMsg(@RequestBody String msg) {
+		System.out.println("Publish message to kafka - " + msg);
+	
+        return msg;
+	}
+}//end of class MS2Controller
